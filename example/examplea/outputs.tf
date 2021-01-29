3dce7d27a8d1540db0ccd99eee7b33e8d4293d18
James Woolfenden <james.woolfenden@gmail.com>
2021-01-29 13:50:42 +0000

    pass pre-commits

--- example/examplea/outputs.tf
+++ example/examplea/outputs.tf
@@ -1,3 +1,13 @@
+output "role_arn" {
+  description = "The cross-account access role ARN for Bridgecrew"
+  value       = module.readonly.role_arn
+}
+
+output "customer_name" {
+  description = "The customer name as defined on Bridgecrew signup"
+  value       = module.readonly.customer_name
+} 
+
 output "role" {
   description = "The cross-account access role for Bridgecrew"
   value       = module.readonly.role
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
+output "role_arn" {
+  description = "The cross-account access role ARN for Bridgecrew"
+  value       = aws_iam_role.bridgecrew_account_role.arn
+}
+
+output "customer_name" {
+  description = "The customer name as defined on Bridgecrew signup"
+  value       = var.org_name
+}  
+
 output "role" {
   description = "The cross-account access role for Bridgecrew"
   value       = aws_iam_role.bridgecrew_account_role
